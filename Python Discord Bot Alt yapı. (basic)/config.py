e1ac938e2ad9f136a18cfde2887dca8398c664b5
Twispz <123075247+twispz@users.noreply.github.com>
2024-01-21 22:10:15 +0300

    Update config.py

--- Python Discord Bot Alt yapı. (basic)/config.py
+++ Python Discord Bot Alt yapı. (basic)/config.py
@@ -1,2 +1,2 @@
-TOKEN = 'MTE5ODY3NDA3ODI0MDg3NDUyOA.G5aTFu.DPWT3Hceq1chse_1wNQvb92iq3ooGNWXokiVFI' # Botun Tokeni Girilecek.
+TOKEN = '' # Botun Tokeni Girilecek.
 PREFIX = "." # Bot Prefix Girilecek.
